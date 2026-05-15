1b5eabe8f2401b547f137480082bf7cd0bafa650
daygool🏁 <daiane.rocha2017@gmail.com>
2026-05-15 13:40:06 -0300

    Refactor Roleta class structure and formatting

--- Roleta.java
+++ Roleta.java
@@ -1,14 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.exemploaula09;
 import java.util.Random;
 
-/**
- *
- * @author daiane.rocha2
- */
 public class Roleta extends Jogo{
 
      @Override
